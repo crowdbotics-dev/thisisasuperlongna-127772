2d9d5a764e0378dbbcda9c2c555eee5b7d65ca6d
Crowdbotics <engineering@crowdbotics.com>
2023-11-10 15:09:25 +0000

    Update store.

--- store/thisisasuperlongnaAPI/api.js
+++ store/thisisasuperlongnaAPI/api.js
@@ -3,25 +3,16 @@ const thisisasuperlongnaAPI = axios.create({
   baseURL: "https://thisisasuperlongna-127772.botics.co",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function api_docs_schema_retrieve(payload) {
+  return thisisasuperlongnaAPI.get(`/api-docs/schema/`, {
+    params: { lang: payload.lang }
+  })
+}
 function api_v1_login_create(payload) {
   return thisisasuperlongnaAPI.post(`/api/v1/login/`, payload)
 }
 function api_v1_signup_create(payload) {
   return thisisasuperlongnaAPI.post(`/api/v1/signup/`, payload)
-}
-function rest_auth_user_retrieve(payload) {
-  return thisisasuperlongnaAPI.get(`/rest-auth/user/`)
-}
-function rest_auth_user_update(payload) {
-  return thisisasuperlongnaAPI.put(`/rest-auth/user/`, payload)
-}
-function rest_auth_user_partial_update(payload) {
-  return thisisasuperlongnaAPI.patch(`/rest-auth/user/`, payload)
-}
-function api_docs_schema_retrieve(payload) {
-  return thisisasuperlongnaAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
 }
 function rest_auth_login_create(payload) {
   return thisisasuperlongnaAPI.post(`/rest-auth/login/`, payload)
@@ -29,20 +20,20 @@ function rest_auth_login_create(payload) {
 function rest_auth_logout_create(payload) {
   return thisisasuperlongnaAPI.post(`/rest-auth/logout/`)
 }
-function rest_auth_registration_create(payload) {
-  return thisisasuperlongnaAPI.post(`/rest-auth/registration/`, payload)
+function rest_auth_password_change_create(payload) {
+  return thisisasuperlongnaAPI.post(`/rest-auth/password/change/`, payload)
 }
 function rest_auth_password_reset_create(payload) {
   return thisisasuperlongnaAPI.post(`/rest-auth/password/reset/`, payload)
-}
-function rest_auth_password_change_create(payload) {
-  return thisisasuperlongnaAPI.post(`/rest-auth/password/change/`, payload)
 }
 function rest_auth_password_reset_confirm_create(payload) {
   return thisisasuperlongnaAPI.post(
     `/rest-auth/password/reset/confirm/`,
     payload
   )
+}
+function rest_auth_registration_create(payload) {
+  return thisisasuperlongnaAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_registration_resend_email_create(payload) {
   return thisisasuperlongnaAPI.post(
@@ -56,19 +47,28 @@ function rest_auth_registration_verify_email_create(payload) {
     payload
   )
 }
+function rest_auth_user_retrieve(payload) {
+  return thisisasuperlongnaAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return thisisasuperlongnaAPI.put(`/rest-auth/user/`, payload)
+}
+function rest_auth_user_partial_update(payload) {
+  return thisisasuperlongnaAPI.patch(`/rest-auth/user/`, payload)
+}
 export const apiService = {
+  api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
-  rest_auth_user_retrieve,
-  rest_auth_user_update,
-  rest_auth_user_partial_update,
-  api_docs_schema_retrieve,
   rest_auth_login_create,
   rest_auth_logout_create,
-  rest_auth_registration_create,
-  rest_auth_password_reset_create,
   rest_auth_password_change_create,
+  rest_auth_password_reset_create,
   rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
   rest_auth_registration_resend_email_create,
-  rest_auth_registration_verify_email_create
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
 }
